4b1b44e45c17bfd45f96125a7638f9fc47dfb3b1
Kalprajsinh <kalpraj51@gmail.com>
2025-04-19 10:53:47 +0530

    backend host

--- backend/api/index.js
+++ backend/api/index.js
@@ -10,7 +10,7 @@ app.use(cors({
     origin: "*"
 }));
 
-app.use("/", require(path.resolve(__dirname, "routes/router.js")));
+app.use("/", require(path.resolve(__dirname, "../routes/router.js")));
 
 app.listen(3001, () => {
     console.log("Server is running on port 3000");
